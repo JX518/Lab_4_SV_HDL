cbf948d3495b7f8d1dcc25d0a5fa2e9e4e5b0383
Justin Xu <xujustin7@gmail.com>
2024-03-04 07:35:16 -0700

    Added instruction memory file initialization to Lab4.sv

--- Lab4.sv
+++ Lab4.sv
@@ -1,7 +1,10 @@
 module Lab4(	    
+output logic y
 );
+logic [11:0] memmory_instruction;
+
 (* ram_init_file = "Lab4.mif" *) logic [11:0] mem[63:0];
+assign memory_instruction = mem[0];
 
-assign *Instruction Code* = mem[*Program Counter*];
-
+assign y = 2'd2;
 endmodule
