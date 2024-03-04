4fa49aef8d06f2ee98f7cc4505a63bc3b18ce098
Justin Xu <xujustin7@gmail.com>
2024-03-03 21:45:54 -0700

    added ALU input output variables

--- ALU.sv
+++ ALU.sv
@@ -7,3 +7,10 @@
 	// loading from mem
 
 	// selecting immediate
+module ALU(
+	input logic [2:0] op,
+	input logic [2:0] ra,
+	input logic [2:0] rb,
+	input logic [2:0] rd,   
+	output logic [5:0] d	  
+);
