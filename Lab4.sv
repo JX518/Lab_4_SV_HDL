5d664320f488e32139b04ac81aa19f3f13af6044
Justin Xu <xujustin7@gmail.com>
2024-03-04 07:51:45 -0700

    added input/output of Lab4 and description of what they do.

--- Lab4.sv
+++ Lab4.sv
@@ -1,10 +1,24 @@
-module Lab4(	    
-output logic y
+module Lab4(
+	input logic  clk, //clock
+	    
+	input logic  SW1, // single step mode
+	    
+ 	input logic  SW2, // used to select output of LEDs
+ 	input logic  SW3,
+	input logic  SW4,
+	    
+	input logic  SW7, // register file //
+	    // (2^3 = 8), there are 8 registers
+	input logic  SW6,
+	input logic  SW5,
+	    
+	input logic  KEY0, // advance in single step mode
+	output logic LED[7:0] // display output according to switches 2, 3, 4.
 );
 logic [11:0] memmory_instruction;
 
 (* ram_init_file = "Lab4.mif" *) logic [11:0] mem[63:0];
 assign memory_instruction = mem[0];
 
-assign y = 2'd2;
+
 endmodule
