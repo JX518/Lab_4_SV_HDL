7b61209858b2e63a6147977de0b8cd4e1755b644
JX518 <Xujustin7@gmail.com>
2024-03-03 22:09:23 -0700

    A bit of work on 333 lab (#4)
    * adding comments to stuff as well as began initiallizing modules
    
    * deleted:    decoder.sv
    
    * added ALU input output variables
    
    * set up the basics of the program counter
    
    * Setting up the lab4 file, im going to sleep soon

--- ALU.sv
+++ ALU.sv
@@ -7,3 +7,11 @@
 	// loading from mem
 
 	// selecting immediate
+
+module ALU(
+	input logic [2:0] op,
+	input logic [2:0] ra,
+	input logic [2:0] rb,
+	input logic [2:0] rd,   
+	output logic [5:0] d	  
+);
