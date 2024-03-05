e3ba65c044c153d3c6bd66af590c1228dbf6c762
Justin Xu <xujustin7@gmail.com>
2024-03-05 09:50:55 -0700

    finished ALU, might have errors

--- ALU.sv
+++ ALU.sv
@@ -9,9 +9,34 @@
 	// selecting immediate
 
 module ALU(
-	input logic [2:0] op,
-	input logic [2:0] ra,
-	input logic [2:0] rb,
-	input logic [2:0] rd,   
+	input logic [2:0]  op,
+	input logic [2:0]  RA,
+	input logic [2:0]  RB,
+	input logic [2:0]  RD,
+	input logic [5:0]  A,
+	input logic [5:0]  B, 
 	output logic [5:0] d	  
 );
+
+   	/* 000	HALT	halt program execution
+	 * 001	LDI	load immediate
+	 * 010	ADD	add
+	 * 011	ADI	add immediate
+	 * 100	MUL 	multiply
+	 * 101	CMPJ	compare jump
+	 * 110	JMP	jump
+	 * 111	NOP	no op
+	 */
+	always_comb begin	   	
+		unique case(op)
+			3'd0: d = 6'b0 
+			3'd1: d = {RA,RB}
+			3'd2: d = A + B
+			3'd3: d = A + RB
+			3'd4: d = A * B
+			3'd5: d = (A >= B) ? RD : 1'b1;
+			3'd6: d = {RA,RB}
+			3'd7: d = 6'b0
+		end
+	endcase
+		
