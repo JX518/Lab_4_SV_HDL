feab267f453f27ef8280ca77b9d554d222cad230
Justin Xu <xujustin7@gmail.com>
2024-03-04 19:53:34 -0700

    Added a lot of modules but haven't set up inputs and outputs, need to add a d flipflop array

--- Lab4.sv
+++ Lab4.sv
@@ -10,7 +10,8 @@
  * 	
  */
 
-module Lab4(	    
+module Lab4(
+	logic input clk,
 	logic input SW0, 
 	logic input SW1, 
 	logic input SW2, 
@@ -21,8 +22,8 @@ module Lab4(
 	logic input SW7,
 	logic input KEY0,
 	
-	logic output [7:0] LED
-	logic output [3:0] 
+	logic output [10:0] LED,
+	logic output [3:0]
 );
 	logic [11:0] instruction;	// 12 but instruction 	
 	logic [5:0]	address; 	// 2^6 = 64
@@ -30,14 +31,50 @@ module Lab4(
 	(* ram_init_file = "Lab4.mif" *) logic [11:0] mem[63:0];
 	
 	assign instruction = mem[address];   
+	d_flipflop_array()
+  	
 
+	register_file(	.data_in(), 
+			.RA(), 
+			.RB(), 
+			.RD(), 
+			.A(),
+			.B()
+	);
    
-	/* Handles debugging switches*/   
+	  
+	ALU(	.op(),
+		.RA(),
+		.RB(),
+		.A(),
+		.B(),
+		.data_out()
+	);
+   
+	 
+	PC(	.clk()
+		.jump()
+		.inc()
+		.count()
+	);
+   
+	
+   
+	/* Handles debugging switches
+	 * 0	Register #{SW7,SW6,SW5}
+	 * 1	Instruction Register
+	 * 2	Program Counter
+	 * 3	opcode
+	 * 4	ALU output
+	 * 5	Debug
+	 * 6	Debug
+	 * 7	Debug
+	 */   
 	always_comb begin 
 		unique case ({SW4,SW3,SW2})
 			3'd0: LED = '0;
 			3'd1: LED = '0;
-			3'd2: LED = '0;
+			3'd2: LED = address;
 			3'd3: LED = '0;
 			3'd4: LED = '0;
 			3'd5: LED = '0;
