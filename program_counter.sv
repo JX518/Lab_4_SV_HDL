41edd3f075778cc9a2585a967f5ad9d0d445f22d
Justin Xu <xujustin7@gmail.com>
2024-03-05 12:28:12 -0700

    added program counter

--- program_counter.sv
+++ program_counter.sv
@@ -1,8 +1,32 @@
-module programCounter(
-	logic input aluAddr [5:0],
-	logic output nextAddr [5:0], //2^6 = 64
-);
-//this module selects between the next address of the program counter or the jump/branch address
-	counter   
+
+module program_counter
+  #(
+    )
+   (
+    input logic [2:0]	opcode,
+    input logic [5:0]	jump_addr,
    
-endmodule
+    input logic		clk,
+    input logic		rst,
+    input logic 	en,
+
+    output logic [63:0]	address 
+    );
+   logic 	jump;
+   logic [6:0] cnt_nxt;
+
+   assign jump = (opcode > 3'b100); // handles jump
+   assign cnt_nxt = (cnt == 6'd63) ? 6'd63 : cnt + 1'b1; // handles count + 1
+
+   dff #(6) myDff( 
+	.d((jump)?
+		jump_addr : 
+		cnt_nxt
+	  ), 
+	.clk(clk), 
+	.rst(rst), 
+	.en(en), 
+	.q(cnt)
+   );
+
+endmodule 
